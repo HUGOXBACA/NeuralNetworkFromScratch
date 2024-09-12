f61e5bb7672c8a3f6d23f34a9ef98faba2362036
Hugo Baca <006434515@coyote.csusb.edu>
2024-09-12 14:43:53 -0700

    we are on our 1st attempt

---
+++ BuildingBlocks/bb_04_0/perceptron.cpp
@@ -0,0 +1,17 @@
+#include "perceptron.h"
+
+	double frand(){
+		return(2.0*(double)rand()/ RAND_MAX) - 1.0 ; 
+	}
+
+	//return new perceptron with number of inputs
+	Perceptron::Perceptron(size_t inputs, double bias){
+
+	}
+
+	//run, x is a vector with input values 
+	double Perceptron::run(std::vector<double> x){
+		return 0.0;
+	}
+
+
